a7d31c3334525b714deef4544eda8d892dd8371c
arafatDU <bsse1435@iit.du.ac.bd>
2025-01-21 23:59:28 +0600

    Socket Connection

--- backend/server/src/index.ts
+++ backend/server/src/index.ts
@@ -1,12 +1,36 @@
-import express from 'express';
+import cors from "cors"
+import dotenv from "dotenv"
+import express, { Express } from "express"
+import fs from "fs"
+import { createServer } from "http"
+import { Server, Socket } from "socket.io"
 
-const app = express();
-const port = 3000;
+// Load environment variables
+dotenv.config()
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the SPL-2 Backend!');
-});
+// Initialize Express app and create HTTP server
+const app: Express = express()
+const port = process.env.PORT || 4000
+app.use(cors())
+const httpServer = createServer(app)
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*", // Allow connections from any origin
+  },
+})
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+// Middleware for socket authentication
+// io.use(socketAuth) // Use the new socketAuth middleware
+
+io.on("connection", (socket: Socket) => {
+  console.log(`New client connected: ${socket.id}`)
+
+  socket.on("disconnect", () => {
+    console.log(`Client disconnected: ${socket.id}`)
+  })
+})
+
+// Start the server
+httpServer.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})
